2354d835f3a23564b314ab95245eac1317149934
Abhijith Selvam <abselvam44@gmail.com>
2026-02-01 19:17:39 +0530

    frontend initial setup

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoginPage() {
+  return (
+    <div>
+      <h1 className="text-white text-5xl">LoginPage</h1>
+    </div>
+  );
+}
+
+export default LoginPage;
